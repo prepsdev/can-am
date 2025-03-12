3160a754c2b281d63d7f3dc914498f816f2fe479
prepsdev <prepsdev.work@gmail.com>
2025-03-12 08:13:41 +0700

    dikit

--- database/migrations/2025_03_10_071756_create_products_table.php
+++ database/migrations/2025_03_10_071756_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('code');
             $table->string('name');
             $table->string('type');
+            $table->bigInteger('price');
             $table->timestamps();
         });
     }
--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -20,7 +20,7 @@
             <div>
                 <div class="brand-logo d-flex align-items-center justify-content-between">
                     <a href="{{ route('admin.dashboard') }}" class="text-nowrap logo-img">
-                        <img src="{{ asset('images/logos/dark-logo.svg') }}" width="180" alt="" />
+                        <img src="{{ asset('images/logos/logo.png') }}" width="180" alt="" />
                     </a>
                     <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                         <i class="ti ti-x fs-8"></i>
